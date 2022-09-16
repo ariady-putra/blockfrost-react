6c10735ee34920629575a93bd844ae11ba492afd
Ariady Putra <ariady.putra@gmail.com>
2022-09-16 23:01:13 +0700

    Add API KEY fallback

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const { BlockFrostAPI } = require('@blockfrost/blockfrost-js');
 async function run(api, rsp) {
-    const json = JSON.stringify(await api, null, 4);
-    console.log(json);
-    rsp.send(json);
+  const json = JSON.stringify(await api, null, 4);
+  console.log(json);
+  rsp.send(json);
 }
 
 var API;
@@ -10,31 +10,31 @@ var API;
 const express = require('express');
 const app     = express();
 const server  = app.listen(55555, () => {
-    const { exec } = require('child_process');
-    exec('cat ~/cardano/cfg/pid.bf', (x,o,e) => {
-        API = new BlockFrostAPI({
-            projectId: o.toString().trim()
-        });
+  const { exec } = require('child_process');
+  exec('cat ~/cardano/cfg/pid.bf', (x,o,e) => {
+    API = new BlockFrostAPI({
+      projectId: o == null ? 'YOUR API KEY' : o.trim()
     });
+  });
 });
 
 app.get('/latestBlock', function(req, rsp) {
-    run(API.blocksLatest(), rsp);
+  run(API.blocksLatest(), rsp);
 });
 
 app.get('/networkInfo', function(req, rsp) {
-    run(API.network(), rsp);
+  run(API.network(), rsp);
 });
 
 app.get('/latestEpoch', function(req, rsp) {
-    run(API.epochsLatest(), rsp);
+  run(API.epochsLatest(), rsp);
 });
 
 app.get('/health', function(req, rsp) {
-    run(API.health(), rsp);
+  run(API.health(), rsp);
 });
 
 app.get('/pools', function(req, rsp) {
-    run(API.pools({ order: 'desc' }), rsp);
+  run(API.pools({ order: 'desc' }), rsp);
 });
 
